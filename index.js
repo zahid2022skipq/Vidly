fc84069a9670bdd4da8b50bd1fa717cba93ced23
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-09 11:51:45 +0500

    Post request to genre done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const genre = [
   { id: 1, name: "g1" },
@@ -12,6 +13,19 @@ const genre = [
 app.get("/vidly/genre", (req, res) => {
   try {
     return res.status(200).send({ message: "Genre", data: genre });
+  } catch (error) {
+    return res.send(error);
+  }
+});
+
+app.post("/vidly/genre", (req, res) => {
+  const { name } = req.body;
+  const genreId = genre.length + 1;
+
+  const newGenre = { id: genreId, name };
+  try {
+    genre.push(newGenre);
+    return res.status(200).send(genre);
   } catch (error) {
     return res.send(error);
   }
