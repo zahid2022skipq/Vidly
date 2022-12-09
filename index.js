4c148eb344ece2461aae1154a3073731299756ae
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-09 11:46:05 +0500

    Get request to vidly genre endpoint

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-import express from "express"
-
+import express from "express";
 
 const app = express();
 
+const genre = [
+  { id: 1, name: "g1" },
+  { id: 2, name: "g2" },
+  { id: 3, name: "g3" },
+  { id: 4, name: "g4" },
+];
 
-app.get("/", (req, res) => {
-    res.send("Working")
-})
+app.get("/vidly/genre", (req, res) => {
+  try {
+    return res.status(200).send({ message: "Genre", data: genre });
+  } catch (error) {
+    return res.send(error);
+  }
+});
 
 app.listen(3000, () => {
-    console.log('====================================');
-    console.log("App Listening on 3000");
-    console.log('====================================');
-})
+  console.log("====================================");
+  console.log("App Listening on 3000");
+  console.log("====================================");
+});
